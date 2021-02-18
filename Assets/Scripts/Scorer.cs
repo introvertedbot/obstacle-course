c6df8b1da42cd9af92544759ee87844cefcefcb7
introvertedbot <introvertedbot@protonmail.com>
2021-02-18 16:33:27 +0530

    Finished obstacle game

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("You've bumped into a thing this many times: " + hits);
+        if(collision.gameObject.tag != "Hit") {
+            hits += 1;
+        }
+        
+    }
+}
